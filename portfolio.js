ba8c6747ef677dee877b54f54b575983c9a16475
SiffKL <siff0407@stud.kea.dk>
2023-01-09 13:53:55 +0100

    nav rettelse og funktion enterSite

--- portfolio.js
+++ portfolio.js
@@ -1,12 +1,8 @@
 window.addEventListener('load', enterSite)
-window.addEventListener('load', knapPuls)
 
 function enterSite() {
   console.log('enterSite')
   document.querySelector('.main').classList.add('fadein')
-}
-
-function knapPuls() {
   console.log('knapPuls')
   document.querySelector('.litemaer').classList.add('pulse')
 }
